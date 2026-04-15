ef5fd10a2487ae4c114de000ff72a60c04c2e004
BENON LUTAAYA <lutaayabenon4@gmail.com>
2026-04-15 16:36:47 +0300

    Update GameController.java

--- src/main/java/com/mycompany/snakesladders/controller/GameController.java
+++ src/main/java/com/mycompany/snakesladders/controller/GameController.java
@@ -2,6 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/**
+ *
+ * @author LUTS
+ */
 package com.mycompany.snakesladders.controller;
 
 import com.mycompany.snakesladders.model.GameState;
